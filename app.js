70b6ee04b55177864088e5834248cb7ea89ffc8b
gregsmith446 <gregsmith446@gmail.com>
2018-01-30 06:44:40 +0000

    finally have a working number 4

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+//requires express and sets it = to app
 var express = require('express');
 var app = express();
-
-app.use(express.static('public'));
-
+//parses the data from the user and makes it useable for us
+//set it equal to parseURLencoded
 var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+var parseUrlEncoded = bodyParser.urlencoded({ extended: false });
 
+//cities object in JSON format
  var cities = {
         'Albany': 'NY',
         'Boston': 'MA',
@@ -13,29 +14,14 @@ var parseUrlencoded = bodyParser.urlencoded({ extended: false });
         'Hartford': 'CT',
         'Concord': 'NH'
    };
-   
-app.post('/cities', parseUrlencoded, function(request, response){
-    var newCity = request.body;
-    cities[newCity.name] = newCity.description;
-    response.status(201).json = newCity.description;
-});
 
-app.delete('/cities/:name', function(request, response) {
-    delete cities[request.cityName];
-    response.sendStatus(200);
-});
-   
-app.param('name', function(request, response,next) {
-    var name = request.params.name;
-    var city = name[0].toUpperCase() + name.slice(1).toLowerCase(); //normalize name
+//WORKS
+//create static middleware = serves files to app from the public directory
+app.use(express.static('public'));
 
-    //request.blockName = block;
-    request.cityName = city
-    
-    next();
-})   
-
-// curl http://localhost:8080/cities?limit=3
+//WORKS
+//get method route to limit the number of cities by using paramaters
+// test with curl http://localhost:8080/cities?limit=3
 app.get('/cities', function(request, response) {
     if (request.query.limit >= 1 && request.query.limit <= 5) {
         response.json(Object.keys(cities).slice(0, request.query.limit));
@@ -46,6 +32,21 @@ app.get('/cities', function(request, response) {
     }
 });   
 
+//WORKS
+//normalize the city name in the URL
+//test with curl http://localhost:8080/cities/nameofcity
+app.param('name', function(request, response,next) {
+    var name = request.params.name;
+    var city = name[0].toUpperCase() + name.slice(1).toLowerCase(); //normalize name
+
+    request.cityName = city
+    
+    next();
+})
+
+//WORKS
+//get method route to show the state for the requested city
+//test with curl http://localhost:8080/cities/nameofcity
 app.get('/cities/:name', function(request, response){
     var description = cities[request.cityName];
     if (!description) {
@@ -55,5 +56,29 @@ app.get('/cities/:name', function(request, response){
     }
 });
 
+//attempting to use global variable to fix app.post
+var length = Object.keys(cities).length; //find the length of the object, needs global scope
 
-app.listen(process.env.PORT)
+// does not work
+//post method route for adding new cities to <UL>
+app.post('/cities', parseUrlEncoded, function(request, response){
+    if (request.body.addCity.length > 4 || request.body.state.length > 2) {    
+        var newCity = request.body;
+        cities[newCity.addCity] = newCity.state;
+        response.status(201).json(newCity.addCity);
+    } else {
+        response.status(400).json("city must be more than 4 characters, state more than 2");
+        alert = "Invalid Entry";
+    }
+    });
+
+//does not work
+//delete method route for deleting cities from <UL>
+app.delete('/cities/:addCity', function(request, response) {
+    delete cities[request.cityName];
+    response.sendStatus(200);
+});
+   
+app.listen(process.env.PORT, function() {
+    console.log("your express app is running");
+});
--- public/client.js
+++ public/client.js
@@ -1,45 +1,65 @@
-/*global $ cities */
+/*global $ cities cityData */
 
-// Lesson 2 - displaying cities in a list
+// Lesson 2 - get method to post the cities in a list on HTML page
 $(function(){
 
     $.get('/cities', appendToList);
-    
+
+    function appendToList(cities) {
+        var list = [];
+        var city, content;
+        for (var i in cities){
+            city = cities[i]; 
+            content = '<a href="/cities/'+ city +'">' + city + '</a>' + '<a href="#" data-city="' + city + '"> X </a>';
+            list.push($('<li>', { html: content }));
+        }
+        $('.cities-list').append(list); 
+    }
+
+    function invalidEntry() {
+        if ($('#city').length <4 || $('#state').length < 2) {
+            alert = "Invalid Entry";
+            return false;
+        }
+    }
+
     $('form').on('submit', function(event) {
         event.preventDefault();
+        console.log("on submit buttom clicked");
+        
+        invalidEntry();
+        console.log("invalid entry form ran");
+        
         var form = $(this);
         var cityData = form.serialize();
         
-    $('.block-list').on('click', 'a[data-block]', function(event){
-        if (!confirm('Are you sure?')) {
-            return false;
-        }
-        
-        var target = $(event.currentTarget);
-        
-        $.ajax ({
-            type: 'DELETE', url: '/cities/' + target.data('block')
+        $.ajax({
+            type: 'POST', 
+            url: '/cities', 
+            data: cityData 
+    }).done(function(cityName){
+        appendToList([cityName]);
+        form.trigger('reset');
+    });  
+
+//jquery onclick function
+$('.cities-list').on('click', '[data-city]', function(event){
+    if (!confirm('Are you sure you want to delete this city?')) {
+        return false;
+    }
+    
+    var target = $(event.currentTarget); //var for the city/link that is clicked
+    
+    $.ajax ({
+        type: 
+        'DELETE', 
+        url: '/cities/' + target.data('city')
         }).done(function() {
             target.parents('li').remove();
         });
     });
 });    
+});
         
-    $.ajax({
-        type: 'POST', url: '/cities', data: cityData 
-    }).done(function(cityName){
-        appendToList([cityName]);
-        form.trigger('reset');
-    });  
-    });
     
-    function appendToList(cities) {
-        var list = [];
-        var content, city;
-        for (var i in cities){
-            city = cities[i]; 
-            content = '<a href="/cities/'+city+'">'+city+'</a>';
-            list.push($('<li>', { html: content }));
-        }
-        $('.cities-list').append(list); 
-    }
+    
